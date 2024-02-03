b20f17957c5608cac1108e7b6dffd5e78172a110
austinqlao <austinql@hawaii.edu>
2024-02-02 15:11:05 -1200

    WOD 6

---
+++ Lab5/Ex1.js
@@ -0,0 +1,8 @@
+age = 20;
+fav_num = 6;
+day_of_birth = 1;
+month_of_birth = 6;
+console.log(
+    age + fav_num / day_of_birth * month_of_birth,
+    (age + fav_num) / day_of_birth * month_of_birth
+    );
